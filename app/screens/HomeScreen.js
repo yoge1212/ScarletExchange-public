d1dc58bca67b929aedd53ac54ab9bee5fd9affa5
poojakedia <poojakedianov@gmail.com>
2024-03-04 21:31:47 -0500

    redirects to home screen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -3,19 +3,16 @@ import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
 //import firestore from '@react-native-firebase/firestore';
 import { getProducts } from '../api/ProductAPI';
 
-
 const HomeScreen = ({ route }) => {
-
     return (
         <SafeAreaView style={styles.Home}>
             <Text style={{
                 fontSize: 30,
                 paddingBottom: 20
             }}>Active Deals Nearby</Text>
-            <ScrollView style={styles.ScrollView}> 
-            <Text> Products to be populated</Text> 
+            <ScrollView style={styles.ScrollView}>
+                <Text> Products to be populated</Text>
             </ScrollView>
-            <TabComp/>
         </SafeAreaView>
     )
 }
